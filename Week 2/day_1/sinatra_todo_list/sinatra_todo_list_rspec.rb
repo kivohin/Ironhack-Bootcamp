e40b54b80392ab58af9085d1bca99e62c7bc1bfc
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-05 20:46:42 +0200

    Add tests

--- Week 2/day_1/sinatra_todo_list/sinatra_todo_list_rspec.rb
+++ Week 2/day_1/sinatra_todo_list/sinatra_todo_list_rspec.rb
@@ -1,5 +1,26 @@
 # Sinatra Todo List RSpec
 
 require "rspec"
-require "./sinatra_todo_list_rspec.rb"
+require "./sinatra_todo_list.rb"
 
+RSpec.describe "Task class methods" do
+	before :each do
+		@task = Task.new("Test")
+	end
+
+	it "complete status should return false when new task is created" do
+		expect(@task.complete).to eq(false)
+	end
+
+	it "should return false whether the task is not completed" do
+		expect(@task.complete?).to eq(false)
+	end
+
+	it "should return true whether the task is completed" do
+		expect(@task.complete?).to be true
+	end
+
+	# it "complete status should return false when new task is created" do
+	# 	expect(Task.new("Test 4").complete!).to eq(false)
+	# end
+end
