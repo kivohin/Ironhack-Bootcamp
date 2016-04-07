c4e23f44285c21950727adf32c9b79f5446e6cbd
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-07 22:24:40 +0200

    Modify testing_sinatra exercises

--- Week 2/day_4/testing_sinatra/server.rb
+++ Week 2/day_4/testing_sinatra/server.rb
@@ -1,1 +1,18 @@
 require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+	"I'm Root!"
+end
+
+# get "/home" do
+# end
+
+get "/real_page" do
+	"I'm Real Page!"
+end
+
+get "/hi" do
+	"I'm Hi!"
+	redirect "/real_page"
+end
