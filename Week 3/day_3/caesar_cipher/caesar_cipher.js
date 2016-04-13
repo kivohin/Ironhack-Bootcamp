5ab938466df6643a94b080e4232457f5642adfab
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-13 20:47:59 +0200

    Create iteration 2, 3 and 4 for Caesar Cipher

--- Week 3/day_3/caesar_cipher/caesar_cipher.js
+++ Week 3/day_3/caesar_cipher/caesar_cipher.js
@@ -1,15 +1,54 @@
-function caesarCipher(message) {
+function caesarCipher(message, shift) {
+	if (shift === undefined) {
+		shift = -3;
+	}
+
 	var ary_letters_original = message.split("");
 
 	var ary_letters_shifted = ary_letters_original.map(function (letter) {
-		var num = letter.charCodeAt(0) - 3;
-		return String.fromCharCode(num);
+		if (letter >= "A" && letter <= "Z") {
+			var num = letter.charCodeAt(0) + shift;
+
+			if (num < 65) {
+				num += 26;
+			}
+
+			else if (num > 90) {
+				num -= 26;
+			}
+
+			return String.fromCharCode(num);
+		}
+
+		else if (letter >= "a" && letter <= "z") {
+			var num = letter.charCodeAt(0) + shift;
+
+			if (num < 97) {
+				num += 26;
+			}
+
+			else if (num > 122) {
+				num -= 26;
+			}
+
+			return String.fromCharCode(num);
+		}
+
+		else {
+			return letter;
+		}
 	});
+
 	var message_shifted = ary_letters_shifted.join("");
 	return message_shifted;
 }
 
-var encrypted = caesarCipher("brutus");
+var encrypted = caesarCipher("Et tu, brute?", 6);
 console.log(encrypted);
 
-// "_orqrp"
+// console.log("a".charCodeAt(0))
+// console.log("z".charCodeAt(0))
+// console.log("A".charCodeAt(0))
+// console.log("Z".charCodeAt(0))
+
+// "_orqrp" - "Bq qr, _orqb?"
