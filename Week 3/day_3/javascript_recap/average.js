766c90ae440467b22cf9ebf0cae68357891652f3
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-13 17:03:54 +0200

    Fix average.js program, now it works

--- Week 3/day_3/javascript_recap/average.js
+++ Week 3/day_3/javascript_recap/average.js
@@ -1,7 +1,7 @@
 var numbers = "80:70:90:100";
 var avg = 0;
 
-function averageColon(string_numbers) {
+function averageColon (string_numbers) {
 	var separatedNumbers = string_numbers.split(":")
 
 	var total_sum = separatedNumbers.reduce(function (sum, num) {
