1e7839eec47d4def142f1c2e0b7e636bd1a63457
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-15 10:38:13 +0200

    Modify vikings.js code, improve program, add several features

--- Week 3/day_4/vikings/vikings.js
+++ Week 3/day_4/vikings/vikings.js
@@ -5,7 +5,7 @@ var Viking = function(name, health, strength) {
 };
 
 var Fight = function(viking_1, viking_2) {
-	console.log("Hey there player! The pit fight is about to begin!\nPrepared for knocking each other up are " + viking_1.name + " and " + viking_2.name + "!\nFight!");
+	console.log("Hey there player! The pit fight is about to begin!\nPrepared for knocking each other up are " + viking_1.name + " and " + viking_2.name + "!\nFight!\n");
 
 	this.attack = function(name) {
 		if (name === viking_1.name) {
@@ -16,12 +16,29 @@ var Fight = function(viking_1, viking_2) {
 			viking_1.health -= viking_2.strength;
 		}
 
-		console.log("Viking " + viking_1.name + " health is " + viking_1.health + ".\nViking " + viking_2.name + " health is " + viking_2.health + ".");
+		while ((viking_1.health - viking_2.strength) > 0 && (viking_2.health - viking_1.strength) > 0) {
+
+			viking_2.health -= viking_1.strength;
+			viking_1.health -= viking_2.strength;
+
+			console.log("Viking " + viking_1.name + " health is " + viking_1.health + ".\nViking " + viking_2.name + " health is " + viking_2.health + ".\n");
+		}
+
+		console.log("And the fight is over!");
+
+		if (viking_1.health > viking_2.health) {
+			console.log(viking_1.name + " is the strongest!");
+		}
+
+		else {
+			console.log(viking_2.name + " is the strongest!");	
+		}
 	};
 };
 
-var viking_anthony = new Viking("Anthony", 100, 7);
-var viking_boomy = new Viking("Boomy", 200, 3);
+var viking_anthony = new Viking("Anthony", 50, 7);
+var viking_boomy = new Viking("Boomy", 77, 3);
 
 var fight_1 = new Fight(viking_anthony, viking_boomy);
-fight_1.attack("Anthony");
+fight_1.attack("Boomy");
+
