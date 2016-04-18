80fe23735edcf28986b19a9f2af8c05b70233810
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-18 11:02:32 +0200

    Add 2 routes, and 2 views

--- Week 4/day_1/ror_project/time_tracking/config/routes.rb
+++ Week 4/day_1/ror_project/time_tracking/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get "/", to: "site#home"
+
+  get "/contact", to: "site#contact"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
