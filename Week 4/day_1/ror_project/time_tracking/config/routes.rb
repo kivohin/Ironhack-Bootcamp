73a8ad6c78e12c10936a597083b098fb35e9227a
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-18 13:05:57 +0200

    Add image to Home Page, and maybe more things

--- Week 4/day_1/ror_project/time_tracking/config/routes.rb
+++ Week 4/day_1/ror_project/time_tracking/config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   get "/contact", to: "site#contact"
 
+  get "/say_name/:name", to: "site#say_name"
+
+  get "/calculator", to: "site#calculator"
+
+  post "/calculate", to: "site#calculate"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
