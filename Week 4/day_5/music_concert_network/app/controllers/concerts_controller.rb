db6b424469e607d5487de8b5f20aec353e7051b1
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-22 21:50:55 +0200

    Add route 'show', 'view', 'create', add action 'show', 'view', 'create', and partials _header, _footer

--- Week 4/day_5/music_concert_network/app/controllers/concerts_controller.rb
+++ Week 4/day_5/music_concert_network/app/controllers/concerts_controller.rb
@@ -2,4 +2,27 @@ class ConcertsController < ApplicationController
 	def index
 		@all_concerts = Concert.all
 	end
+
+	def show
+		@concert_id = params[:id]
+		@concert_object = Concert.find_by(id: @concert_id)
+	end
+
+	def new
+		@concert_new = Concert.new
+	end
+
+	def create
+		@concert_new = Concert.new(
+			:artist => params[:concert][:artist],
+			:venue => params[:concert][:venue],
+			:city => params[:concert][:city],
+			:date => params[:concert][:date],
+			:price => params[:concert][:price],
+			:description => params[:concert][:description])
+
+		@concert_new.save
+
+		redirect_to '/index'
+	end
 end
--- Week 4/day_5/music_concert_network/config/routes.rb
+++ Week 4/day_5/music_concert_network/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   get '/index' => 'concerts#index'
 
+  get '/show/:id' => 'concerts#show'
+
+  get '/new' => 'concerts#new'
+
+  post '/create' => 'concerts#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
