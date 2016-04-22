46b6fc5a858c8481ddde1fc1eac6ee41b63c2ea5
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-22 17:34:36 +0200

    Add route 'index', add action 'index', and add view 'index'

--- Week 4/day_5/music_concert_network/app/controllers/concerts_controller.rb
+++ Week 4/day_5/music_concert_network/app/controllers/concerts_controller.rb
@@ -1,2 +1,5 @@
 class ConcertsController < ApplicationController
+	def index
+		@all_concerts = Concert.all
+	end
 end
--- Week 4/day_5/music_concert_network/db/seeds.rb
+++ Week 4/day_5/music_concert_network/db/seeds.rb
@@ -7,3 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Concert.create artist: 'Michael Jackson', venue: 'Batcave', city: 'Bucaramanga', date: '2016-04-13', price: 77, description: 'Awesome last concert of MJ!'
+Concert.create artist: 'Carlos Vives', venue: 'Secret mansion', city: 'Santa Marta', date: '2015-05-07', price: 50, description: 'Super latin concert in a hidden magic mansion!'
+Concert.create artist: 'Metronomy', venue: "Spiderman's house", city: 'New York', date: '2020-03-24', price: 69, description: "Never seen spectacular concert at Spidy's!"
