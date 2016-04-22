2d4d93ae12929aa764648a75dcded4233d7c9822
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-22 16:54:12 +0200

    Modify seed file in which add example info for DB

--- Week 4/day_5/music_concert_network/db/seeds.rb
+++ Week 4/day_5/music_concert_network/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Concert.create artist: 'Michael Jackson', venue: 'Batcave', city: 'Bucaramanga', date: '2016-04-13', price: 77, description: 'Awesome last concert of MJ!'
