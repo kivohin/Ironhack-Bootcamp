5feb18f1f6b8c3cdcd7be55ff4369a6422e52102
Anthony Nabuurs <kivohin@protonmail.com>
2016-04-25 21:20:13 +0200

    Finish iteration 2 from Favourite Phrases exercise

--- Week 5/day_1/favourite_phrases/js/app.js
+++ Week 5/day_1/favourite_phrases/js/app.js
@@ -2,13 +2,13 @@ $(document).on('ready', function() {
 	var phrases = [
 	'I like chocolate!',
 	'I love Dragon Ball!',
-	'I like playing Pokemon!'
+	'I like playing Pokemon!',
+	'Fuck the system!',
+	'I\'m gonna leave this world in a better place.'
 	];
 
-	function newPhrase() {
-		randomNum = Math.floor(Math.random() * 3);
+	$('button').on('click', function newPhrase() {
+		randomNum = Math.floor(Math.random() * 5);
 		$('p').text(phrases[randomNum]);
-	};
-
-	newPhrase();
+	})
 });
