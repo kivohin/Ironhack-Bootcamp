3ebfeac4a24c78e393174de61f648a126d3203cf
Anthony Nabuurs <kivohin@protonmail.com>
2016-06-14 23:04:08 +0200

    Insert in links_controller.rb call to method search_original_link from model link.rb

--- Week 4/extra/url_shortener/app/controllers/links_controller.rb
+++ Week 4/extra/url_shortener/app/controllers/links_controller.rb
@@ -1,8 +1,6 @@
 class LinksController < ApplicationController
   def search
     short_link = 'localhost:3000/' + params[:shortlink]
-    link_id = Link.find_by(short_URL: short_link).id
-    original_link = Link.find_by(id: link_id).original_URL
-    redirect_to 'http://' + original_link
+   	Link.search_original_link(short_link)
   end
 end
