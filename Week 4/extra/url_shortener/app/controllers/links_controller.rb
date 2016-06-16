2e258a010f57659dba4af50378354c20d055d4b8
Anthony Nabuurs <kivohin@protonmail.com>
2016-06-16 18:55:57 +0200

    Put logic from controller to a method in model, and connect controller and model. Now iteration 1 is complete.

--- Week 4/extra/url_shortener/app/controllers/links_controller.rb
+++ Week 4/extra/url_shortener/app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   def search
     short_link = 'localhost:3000/' + params[:shortlink]
-   	Link.search_original_link(short_link)
+   	redirect_to 'http://' + Link.search_original_link(short_link)
   end
 end
--- Week 4/extra/url_shortener/app/models/link.rb
+++ Week 4/extra/url_shortener/app/models/link.rb
@@ -2,6 +2,6 @@ class Link < ActiveRecord::Base
 	def self.search_original_link(s_l)
 		link_id = Link.find_by(short_URL: s_l).id
     original_link = Link.find_by(id: link_id).original_URL
-    redirect_to 'http://' + original_link
+    return original_link
 	end
 end
